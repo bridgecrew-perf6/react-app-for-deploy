8b65bbb74e1f0fc9b9e363b609cafc27458b518a
irvinpinedaUCA <00024420@uca.edu.sv>
2022-06-14 16:02:22 -0600

    hello_word

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./logo192.png" className="App-logo" alt="logo" />
+        <p>
+        “That's one small step for man, one giant leap for mankind”
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to NASA
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
